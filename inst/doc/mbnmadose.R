ede87a495908ee1bfa319e331d986ae408b859eb
Hugo Pedder <hugopedder@gmail.com>
2022-02-24 10:40:02 +0000

    version 0.4.1

--- inst/doc/mbnmadose.R
+++ inst/doc/mbnmadose.R
@@ -16,31 +16,28 @@ knitr::opts_chunk$set(
 )
 
 ## ---- echo=FALSE--------------------------------------------------------------
-kable(head(HF2PPITT), digits=2) 
-
-## ---- echo=FALSE--------------------------------------------------------------
-kable(head(psoriasis), digits=2) 
+kable(head(triptans), digits=2) 
 
 ## ---- echo=TRUE---------------------------------------------------------------
 kable(head(ssri), digits=2) 
 
 ## ---- echo=FALSE--------------------------------------------------------------
-kable(head(GoutSUA_2wkCFB), digits=2) 
+kable(head(gout), digits=2) 
 
 ## ---- echo=FALSE--------------------------------------------------------------
-kable(head(osteopain_2wkabs), digits=2) 
+kable(head(osteopain), digits=2) 
 
 ## ---- echo=FALSE--------------------------------------------------------------
 kable(head(alog_pcfb), digits=2) 
 
 ## -----------------------------------------------------------------------------
 # Using the triptans dataset
-network <- mbnma.network(HF2PPITT)
+network <- mbnma.network(triptans)
 summary(network)
 
 ## ---- message=FALSE, warning=FALSE--------------------------------------------
 # Prepare data using the gout dataset
-goutnet <- mbnma.network(GoutSUA_2wkCFB)
+goutnet <- mbnma.network(gout)
 summary(goutnet)
 
 ## -----------------------------------------------------------------------------
@@ -71,27 +68,24 @@ plot(nma.alog)
 
 ## ---- results="hide", warning=FALSE-------------------------------------------
 # Run an Emax dose-response MBNMA
-mbnma <- mbnma.run(tripnet, fun="emax", 
-                   beta.1="rel", beta.2="rel", method="common")
+mbnma <- mbnma.run(tripnet, fun=demax(emax="rel", ed50="rel"), 
+                   method="random")
 
 ## ---- message=FALSE, warning=FALSE--------------------------------------------
+# Print neat summary of output
 summary(mbnma)
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  # An alternative would be to use an Emax wrapper for mbnma.run() which would give the
-#  #same result but with more easily interpretable parameter names
-#  mbnma.emax(tripnet, emax="rel", ed50="rel", method="common")
 
 ## ---- results="hide"----------------------------------------------------------
 # Emax model with single parameter estimated for Emax
-emax <- mbnma.emax(tripnet, emax="rel", ed50="common", method="random")
+emax <- mbnma.run(tripnet, fun=demax(emax="rel", ed50="common"), 
+                  method="random")
 
 ## -----------------------------------------------------------------------------
 summary(emax)
 
 ## ---- results="hide", message=FALSE, warning=FALSE----------------------------
 # Using the osteoarthritis dataset
-pain.df <- osteopain_2wkabs
+pain.df <- osteopain
 
 # Set class equal to agent for all agents
 pain.df$class <- pain.df$class
@@ -100,46 +94,43 @@ pain.df$class <- pain.df$class
 pain.df$class[pain.df$agent %in% c("Naproxcinod", "Naproxen")] <- 
   "NSAID"
 
-# Run a restricted cubic spline MBNMA with a common class effect on beta.1
+# Run a restricted cubic spline MBNMA with a random class effect on beta.1
 classnet <- mbnma.network(pain.df)
-splines <- mbnma.run(classnet, fun="rcs", class.effect = list(beta.1="common"))
+splines <- mbnma.run(classnet, fun=dspline(type="bs", knots=2), class.effect = list(beta.1="random"))
 
 ## ---- eval=FALSE--------------------------------------------------------------
-#  # Define relative magnitudes of Emax and ED50
-#  rel.size <- c(4,1)
-#  
-#  mbnma.emax(tripnet, emax="rel", ed50="rel", method="random",
-#             var.scale=rel.size)
-
-## ---- eval=FALSE--------------------------------------------------------------
+#  # Using the depression SSRI dataset
 #  depnet <- mbnma.network(ssri)
 #  
 #  # An example specifying a quadratic dose-response function
-#  quad <- ~ (beta.1 * dose) + (beta.2 * (dose^2))
+#  quadfun <- ~ (beta.1 * dose) + (beta.2 * (dose^2))
 #  
-#  mbnma.run(depnet, fun="user", user.fun=quad,
-#            beta.1 = "rel", beta.2 = "rel")
+#  quad <- mbnma.run(depnet, fun=duser(fun=quadfun, beta.1 = "rel", beta.2 = "rel"))|
 
 ## ---- eval=FALSE--------------------------------------------------------------
-#  # Placebo can be modeled using any function (since it will evaluate to 0)
-#  # Adalimubab and Guselkumab: exponential function (limited dose-response info)
-#  # All others: restricted cubic spline with 3 knots
-#  dr.funs <- rep(NA, length(psorinet$agents))
-#  dr.funs[which(psorinet$agents %in% c("Placebo", "Adalimumab", "Guselkumab"))] <-
-#    "exponential"
-#  dr.funs[which(!psorinet$agents %in% c("Placebo", "Adalimumab", "Guselkumab"))] <-
-#    "rcs"
+#  # Using the depression SSRI dataset
+#  depnet <- mbnma.network(ssri)
 #  
-#  multifun <- mbnma.run(psorinet, fun=dr.funs, method="common", knots=3, n.iter=50000)
+#  dr.funs <- dmulti(list(
+#    "Placebo"=dpoly(degree=2),
+#    "citalopram"=dpoly(degree=2),
+#    "escitalopram"=dpoly(degree=2),
+#    "fluoxetine"=dspline(type="ns",knots=2),
+#    "paroxetine"=dpoly(degree=2),
+#    "sertraline"=dspline(type="ns",knots=2)
+#    ))
+#  
+#  multifun <- mbnma.run(depnet, fun=dr.funs, method="common", n.iter=50000)
 #  summary(multifun)
 
 ## ---- eval=FALSE--------------------------------------------------------------
-#  knots <- 3
+#  dspline(type="bs", knots=3)
 #  # ...is equivalent to
-#  knots <- c(0.25,0.5,0.75)
+#  dspline(type="bs", knots=c(0.25,0.5,0.75))
 #  
+#  # Using a natural cubic spline on the SSRI dataset
 #  depnet <- mbnma.network(ssri)
-#  mbnma.run(depnet, fun="rcs", knots=knots)
+#  ns <- mbnma.run(depnet, fun=dspline(type="ns", knots=c(0.25,0.5,0.75)))
 
 ## -----------------------------------------------------------------------------
 print(mbnma$model.arg$priors)
@@ -151,13 +142,13 @@ print(mbnma$model.arg$priors)
 #    )
 #  
 #  # Run an MBNMA model with new priors
-#  emax <- mbnma.emax(alognet, emax="rel", ed50="rel", method="random",
+#  emax <- mbnma.run(alognet, fun=demax(), method="random",
 #                     priors=new.priors)
 
 ## ---- message=FALSE, warning=FALSE--------------------------------------------
 # Generate dataset without placebo
 noplac.gout <- 
-  GoutSUA_2wkCFB[!GoutSUA_2wkCFB$studyID %in% c(2001, 3102),] # Drop two-arm placebo studies
+  gout[!gout$studyID %in% c(2001, 3102),] # Drop two-arm placebo studies
 noplac.gout <- 
   noplac.gout[noplac.gout$agent!="Plac",] # Drop placebo arm from multi-arm studies
 
@@ -183,15 +174,15 @@ plot(noplac.net, level="agent", remove.loops = TRUE, label.distance = 4,
 plot(noplac.net, level="agent", remove.loops = TRUE, label.distance = 4,
      doselink=3)
 
-## ---- results="hide"----------------------------------------------------------
-nonparam <- mbnma.run(tripnet, fun="nonparam.up", method="random")
+## ---- nonparam, results="hide"------------------------------------------------
+nonparam <- mbnma.run(tripnet, fun=dnonparam(direction="increasing"), method="random")
 
 ## -----------------------------------------------------------------------------
 print(nonparam)
 
 ## ---- results="hide"----------------------------------------------------------
-tripnet <- mbnma.network(HF2PPITT)
-trip.emax <- mbnma.emax(tripnet, emax="rel", ed50="rel") 
+tripnet <- mbnma.network(triptans)
+trip.emax <- mbnma.run(tripnet, fun=demax(emax="rel", ed50="rel")) 
 
 ## ---- results="hide"----------------------------------------------------------
 # Plot boxplots of residual deviance contributions (scatterplot is the default)
@@ -205,23 +196,37 @@ fitplot(trip.emax)
 plot(trip.emax)
 
 ## -----------------------------------------------------------------------------
-ranks <- rank(trip.emax, direction = 1)
+# Specify treatments (agents and doses) for which to estimate relative effects
+treats <- list("Placebo"=0,
+               "eletriptan"= 1,
+               "sumatriptan"=2,
+               "almotriptan"=1)
+
+# Print relative effects on the natural scale
+rels <- get.relative(trip.emax, treatments = treats, eform=TRUE)
+print(rels)
+
+# Rank relative effects
+rank(rels)
+
+## -----------------------------------------------------------------------------
+ranks <- rank(trip.emax, lower_better = FALSE)
 print(ranks)
 summary(ranks)
 
 ## -----------------------------------------------------------------------------
 # Ranking histograms for Emax
-plot(ranks, params = "d.emax")
+plot(ranks, params = "emax")
 
 # Ranking histograms for ED50
-plot(ranks, params = "d.ed50")
+plot(ranks, params = "ed50")
 
 ## -----------------------------------------------------------------------------
 # Cumulative ranking plot for both dose-response parameters
 cumrank(ranks, sucra=TRUE)
 
 ## ---- eval=FALSE--------------------------------------------------------------
-#  E0 <- HF2PPITT[HF2PPITT$dose==0,]
+#  E0 <- triptans[triptans$dose==0,]
 
 ## -----------------------------------------------------------------------------
 # Predict 20 doses for each agent, with a stochastic distribution for E0
@@ -238,7 +243,7 @@ pred <- predict(trip.emax, E0="rbeta(n, shape1=2, shape2=10)",
 
 
 # Predict exact doses for two agents, and estimate E0 from the data
-E0.data <- HF2PPITT[HF2PPITT$dose==0,]
+E0.data <- triptans[triptans$dose==0,]
 doses <- list("eletriptan"=c(0,1,3),
                   "sumatriptan"=c(0,3))
 
@@ -260,7 +265,7 @@ plot(pred, disp.obs = TRUE)
 
 ## ---- results="hide", warning=FALSE, message=FALSE----------------------------
 alognet <- mbnma.network(alog_pcfb)
-alog.emax <- mbnma.emax(alognet, method="random")
+alog.emax <- mbnma.run(alognet, fun=demax(), method="random")
 pred <- predict(alog.emax, E0=0, n.dose=20)
 plot(pred, overlay.split = TRUE, method="random")
 
@@ -288,6 +293,10 @@ kable(data.frame(
 ), digits=2,
 col.names=c("Model", "Residual Deviance", "Betwen-study SD"))
 
+## ---- results="hide", warning=FALSE, message=FALSE----------------------------
+# Compares residual deviance contributions from NMA and UME models
+devdev(nma, ume, dev.type="resdev")
+
 ## ---- results="hide", warning=FALSE, message=FALSE, fig.show = "hide", eval=FALSE----
 #  # Using the psoriasis dataset (>75% improvement in PASI score)
 #  psoriasis$r <- psoriasis$r75
@@ -299,7 +308,7 @@ col.names=c("Model", "Residual Deviance", "Betwen-study SD"))
 #  
 #  # If we want to fit an Emax dose-response function, there is insufficient
 #  #indirect evidence in all but the first 6 comparisons
-#  nodesplit <- mbnma.nodesplit(psorinet, fun="emax", comparisons=splitcomps[1:6,], method="common")
+#  nodesplit <- mbnma.nodesplit(psorinet, fun=demax(), comparisons=splitcomps[1:6,], method="common")
 
 ## ---- eval=FALSE--------------------------------------------------------------
 #  print(nodesplit)
